3479202ed56b2d5c7c937e8affd1f326472b4067
VERT9 <vert9x@gmail.com>
2026-05-21 00:41:52 -0500

    Updated the web build to use a similar flow as the desktop version

--- src/platform_web.c
+++ src/platform_web.c
@@ -4,21 +4,52 @@
 #include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
 #include "game_ui.h"
 #include "asset_sfx.h"
-#include "game_process.h"
+#include "process_define.h"
 #include "scene_loader.h"
+#include "util_observe.h"
 
 #include "rlgl.h"
 #include <emscripten/emscripten.h>
 
-float screenWidth = 1280.0f;
-float screenHeight = 720.0f;
+static int screenWidth = 1280;
+static int screenHeight = 720;
+static bool windowReady = false;
+
+static void NotifyWindowResize(void) {
+  window_resize_t resize = { screenWidth, screenHeight };
+  SubjectNotify("WINDOW", &resize);
+}
+
+// EMSCRIPTEN_KEEPALIVE makes it callable from JS
+EMSCRIPTEN_KEEPALIVE
+void OnCanvasResize(int width, int height) {
+  if (width <= 0 || height <= 0)
+    return;
+
+  screenWidth = width;
+  screenHeight = height;
+
+  if (windowReady) {
+    SetWindowSize(screenWidth, screenHeight);
+    NotifyWindowResize();
+  }
+}
+
 LoadQueue loader = {0};
+int tarFPS = 60;
+bool resLoaded = false;
 
 void UpdateDrawFrame(void);
 int main(void){
+  GameSetFrameRate(tarFPS);
   srand((unsigned int)time(NULL));  // seed once using current time
 
   InitScreenWindow(screenWidth,screenHeight, "raylib game template");
+  windowReady = true;
+  SetWindowSize(screenWidth, screenHeight);
+  NotifyWindowResize();
+
+  GameSetScreen(SCREEN_LOGO);
   InitAudioDevice();      // Initialize audio device
 
   SpriteLoadSplash("resources/splash.png", VEC_NEW(screenWidth,screenHeight));
@@ -35,10 +66,11 @@ int main(void){
 
   InitResources();
   InitUI();
-  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-  CloseAudioDevice();     // Close audio context
 
-  CloseWindow();    
+  resLoaded = true;
+  GameTransitionScreen();
+
+  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 
   return 0;
 }
